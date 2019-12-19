9e21dcacb8def612b88231b10d527bfc64b320f8
Umair Gillani <umairgillani93@gmail.com>
2019-12-19 16:37:33 +0500

    Python CV Scrapper

--- cv_scrapper.py
+++ cv_scrapper.py
@@ -1,12 +1,21 @@
 from pyresparser import ResumeParser
+import pandas as pd
 import nltk
 import spacy
+import os
 
 nlp = spacy.load('en_core_web_sm')
 nltk.download('stopwords')
 
 # Cv information
-data = ResumeParser("new_updated_CV.pdf").get_extracted_data()
+data = ResumeParser("CVAsadjaved.pdf").get_extracted_data()
 
 # Extracting skills from Cv
 print(data['skills'])
+
+# Creating Pandas DataFrame from parsed information
+df = pd.DataFrame({key: pd.Series(value) for key, value in data.items()})
+print(df.head())
+
+# Creating csv file from pandas DataFrame
+df.to_csv(os.getcwd() + "/resume1.csv")
